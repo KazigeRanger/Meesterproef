ef79a06ab89ee7df7f6815cd5a5395f419aa713a
Renger Bierenga <bierengarenger@gmail.com>
2024-10-06 14:10:33 +0200

    Updated the perimeterCalc.js file so that it correctly calculates all the perimeters of the airfoil sections demarcated by the spars and the leading and trailing edges.

--- Algemeen/perimeterCalc.js
+++ Algemeen/perimeterCalc.js
@@ -21,20 +21,22 @@ function calculateAirfoilPerimeter(frontSparX, rearSparX) {
     let enclosedBottomPerimeter = 0;
     let rearBottomPerimeter = 0;
 
-    let upperSkin;
-    let lowerSkin;
+    let currentUpperSkin;
+    let currentLowerSkin;
+    let nextUpperSkin;
+    let nextLowerSkin;
 
     // Calculate the distances between each airfoil point
     for (let i = 0; i < SD7037_x.length; i++) {
         if (i < 31) {
-            upperSkin = true;
-            lowerSkin = false;
+            currentUpperSkin = true;
+            currentLowerSkin = false;
         } else if (i > 31) {
-            upperSkin = false;
-            lowerSkin = true;
+            currentUpperSkin = false;
+            currentLowerSkin = true;
         } else if (i === 31 || i === 0) {
-            upperSkin = true;
-            lowerSkin = true;
+            currentUpperSkin = true;
+            currentLowerSkin = true;
         }
 
         if (i+1 !== SD7037_x.length) {
@@ -46,36 +48,118 @@ function calculateAirfoilPerimeter(frontSparX, rearSparX) {
             totalPerimeter += distance;
 
             // Rear top perimeter conditional
-            if (SD7037_x[i] > rearSparX && SD7037_x[i+1] > rearSparX && upperSkin === true) {
+            if (SD7037_x[i] > rearSparX && SD7037_x[i+1] > rearSparX && currentUpperSkin === true) {
                 rearTopPerimeter += distance;
+                
                 // Rear top perimeter edge case conditional
-            } else if (SD7037_x[i] > rearSparX && SD7037_x[i+1] < rearSparX && upperSkin === true) {
+                // This conditional also contains the enclosed top perimeter rear spar edga case
+                // because the conditionals are the same
+            } else if (SD7037_x[i] > rearSparX && SD7037_x[i+1] < rearSparX && currentUpperSkin === true) {
+                // Rear top perimeter edge case
                 deltaX = SD7037_x[i]-rearSparX;
                 deltaY = SD7037_y[i]-rearTopY;
 
                 distance = Math.sqrt(Math.pow(deltaX, 2)+Math.pow(deltaY, 2));
 
                 rearTopPerimeter += distance;
-                // Enclosed top perimeter conditional
-            } else if (SD7037_x[i] < rearSparX && SD7037_x[i+1] < rearSparX && SD7037_x[i] > frontSparX && SD7037_x[i+1] > frontSparX && upperSkin === true) {
-                enclosedTopPerimeter += distance;
-                // Enclosed top perimeter rear spar edge case conditional
-            } else if (SD7037_x[i] < rearSparX && SD7037_x[i-1] > rearSparX && upperSkin === true) {
-                deltaX = SD7037_x[i]-rearSparX;
-                deltaY = SD7037_y[i]-rearTopY;
+
+                // Enclosed top perimeter rear spar edge case
+                deltaX = rearSparX-SD7037_x[i+1];
+                deltaY = rearTopY-SD7037_y[i+1];
 
                 distance = Math.sqrt(Math.pow(deltaX, 2)+Math.pow(deltaY, 2));
 
                 enclosedTopPerimeter += distance;
+                
+
+                // Enclosed top perimeter conditional
+            } else if (SD7037_x[i] < rearSparX && SD7037_x[i+1] < rearSparX && SD7037_x[i] > frontSparX && SD7037_x[i+1] > frontSparX && currentUpperSkin === true) {
+                enclosedTopPerimeter += distance;
+
                 // Enclosed top perimeter front spar edge case conditional
-            } else if (SD7037_x[i] > frontSparX && SD7037_x[i+1] < frontSparX && upperSkin === true) {
+                // This conditional also contains the front top perimeter edge case conditional
+                // because they have the same conditional
+            } else if (SD7037_x[i] > frontSparX && SD7037_x[i+1] < frontSparX && currentUpperSkin === true) {
+                // Enclosed top perimeter front spar edge case
                 deltaX = SD7037_x[i]-frontSparX;
                 deltaY = SD7037_y[i]-frontTopY;
 
                 distance = Math.sqrt(Math.pow(deltaX, 2)+Math.pow(deltaY, 2));
 
                 enclosedTopPerimeter += distance;
+
+                // Front top perimeter edge case
+                deltaX = frontSparX-SD7037_x[i+1];
+                deltaY = frontTopY-SD7037_y[i+1];
+
+                distance = Math.sqrt(Math.pow(deltaX, 2)+Math.pow(deltaY, 2));
+
+                frontTopPerimeter += distance;
+
+                // Front top perimeter conditional
+            } else if (SD7037_x[i] < frontSparX && SD7037_x[i+1] < frontSparX && SD7037_y[i+1] > 0.01 && currentUpperSkin === true) {
+                frontTopPerimeter += distance;
+                
+                
+                // Front top perimeter edge case conditional
+            } else if (SD7037_x[i] < frontSparX && SD7037_x[i+1] < frontSparX && currentLowerSkin === true) {
+                frontBottomPerimeter += distance;
+                
+
+                // Front bottom perimeter edge case conditional
+                // This conditional also contains the enclosed bottom perimeter front spar 
+                // edge case, because the conditionals are the same
+            } else if (SD7037_x[i] < frontSparX && SD7037_x[i+1] > frontSparX && currentLowerSkin === true) {
+                // Front bottom perimeter edge case
+                deltaX = frontSparX-SD7037_x[i];
+                deltaY = frontBottomY-SD7037_y[i];
+
+                distance = Math.sqrt(Math.pow(deltaX, 2)+Math.pow(deltaY, 2));
+
+                frontBottomPerimeter += distance;
+
+                // Enclosed bottom perimeter front spar edge case
+                deltaX = SD7037_x[i+1]-frontSparX;
+                deltaY = SD7037_y[i+1]-frontBottomY;
+
+                distance = Math.sqrt(Math.pow(deltaX, 2)+Math.pow(deltaY, 2));
+
+                enclosedBottomPerimeter += distance;
+                
+
+                // Enclosed bottom perimeter conditional
+            } else if (SD7037_x[i] > frontSparX && SD7037_x[i+1] < rearSparX && currentLowerSkin === true) {
+                enclosedBottomPerimeter += distance;
+                
+
+                // Enclosed bottom perimeter rear spar edge case conditional
+                // This conditional also contains the rear bottom perimeter rear spar edge case
+                // because the conditionals are the same
+            } else if (SD7037_x[i] < rearSparX && SD7037_x[i+1] > rearSparX && currentLowerSkin === true) {
+                // Enclosed bottom perimeter rear spar edge case
+                deltaX = rearSparX-SD7037_x[i];
+                deltaY = rearBottomY-SD7037_y[i];
+
+                distance = Math.sqrt(Math.pow(deltaX, 2)+Math.pow(deltaY, 2));
+
+                enclosedBottomPerimeter += distance;
+
+                // Rear bottom perimeter rear spar edge case
+                deltaX = SD7037_x[i+1]-rearSparX;
+                deltaY = SD7037_y[i+1]-rearBottomY;
+
+                distance = Math.sqrt(Math.pow(deltaX, 2)+Math.pow(deltaY, 2));
+
+                rearBottomPerimeter += distance;
+                
+
+                // Rear bottom perimeter conditional
+            } else if (SD7037_x[i] > rearSparX && currentLowerSkin === true) {
+                rearBottomPerimeter += distance;
+
+                
             }
+            // Total perimeter edge case
         } else if (i+1 === SD7037_x.length) {
             let deltaX = SD7037_x[i]-SD7037_x[0];
             let deltaY = SD7037_y[i]-SD7037_y[0];
@@ -86,11 +170,26 @@ function calculateAirfoilPerimeter(frontSparX, rearSparX) {
         }
     }
 
-    console.log("");
-    console.log(`The total perimeter of the airfoil is ${totalPerimeter} meters`);
+    console.log(`\nThe total perimeter of the airfoil is ${totalPerimeter} meters`);
     console.log(`-- The perimeter of the rear top section of the airfoil is ${rearTopPerimeter} meters`);
     console.log(`-- The perimeter of the enclosed top section of the airfoil is ${enclosedTopPerimeter} meters`);
-    console.log(`\n${SD7037_x.indexOf(Math.min(...SD7037_x))}`);
+    console.log(`-- The perimeter of the front top section of the airfoil is ${frontTopPerimeter} meters`);
+    console.log(`-- The perimeter of the front bottom section of the airfoil is ${frontBottomPerimeter} meters`);
+    console.log(`-- The perimeter of the enclosed bottom section of the airfoil is ${enclosedBottomPerimeter} meters`);
+    console.log(`-- The perimeter of the rear bottom section of the airfoil is ${rearBottomPerimeter} meters`);
+    console.log(`\nCONTROL. THE SUMMED PERIMETER OF THE SUBSECTIONS IS: ${rearTopPerimeter+enclosedTopPerimeter+frontTopPerimeter+frontBottomPerimeter+enclosedBottomPerimeter+rearBottomPerimeter} meters\nTHE SUMMED PERIMETER OF THE SUBSECTIONS SHOULD BE THE SAME AS THE TOTAL PERIMETER!`);
+
+    const error = 0.0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001;
+    if (rearTopPerimeter+enclosedTopPerimeter+frontTopPerimeter+frontBottomPerimeter+enclosedBottomPerimeter+rearBottomPerimeter >= totalPerimeter-error || rearTopPerimeter+enclosedTopPerimeter+frontTopPerimeter+frontBottomPerimeter+enclosedBottomPerimeter+rearBottomPerimeter <= totalPerimeter+error) {
+        console.log("\nHET WERKT!");
+    } else {
+        console.log("\nHET WERKT NIET");
+    }
 }
 
 module.exports = {calculateAirfoilPerimeter};
+
+// TROUBLESHOOTING
+// Ik denk dat het probleem bij de enclosed top perimeter calculations zit, want de enclosed top perimeter
+// is een stuk kleiner dan de enclosed bottom perimeter, terwijl je zou verwachten dat ze bijna hetzelfde
+// zijn als je kijkt naar de vorm van de airfoil.
